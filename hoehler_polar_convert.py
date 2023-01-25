e039a0a2ec510a8e34d7315550d36adbd41a1997
dhoehler <dhoehler@haverford.edu>
2023-01-25 11:50:02 -0500

    Hoehler edit

---
+++ hoehler_polar_convert.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+r=input("How far away is the point of interest?")
+theta=input("What is the angle?")
+
+x=float(r)*np.cos(float(theta))
+y=float(r)*np.sin(float(theta))
+print("x=",x,"and y=",y)
+print("I am gonna eat a bunch of tendies today")
