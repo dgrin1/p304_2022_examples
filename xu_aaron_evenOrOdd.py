00d24b3861f5f4eb56b9a8f437b1c19b2c16c3ae
Your Name Comes Here <your@yourdomain.example.com>
2023-01-30 10:59:45 -0500

    Updated

--- xu_aaron_evenOrOdd.py
+++ xu_aaron_evenOrOdd.py
@@ -1,9 +1,13 @@
-numbr = input("What number would you like to assess?")
-try:
-    numbr = int(numbr)
-except:
-    print("You did not enter a number, please enter an integer number")
-    numbr = input("What number would you like to assess?")
+def takeUserInput():
+    numbr = input('What number would you like to assess? \n  ')
+    try:
+        numbr = int(numbr)
+    except:
+        print("WARNING: You did not enter a number. Please enter an integer number\n*\n")
+        numbr = takeUserInput()
+    return numbr
+
+numbr = takeUserInput()
 if(numbr%2==1):
     print("Your number is odd.")
 elif(numbr%2==0):
