a60234215d7dcde6e689b1aeaf3eff157e6dd5ef
Seamus Flannery <seamusmflannery@gmail.com>
2023-01-30 10:42:39 -0500

    Made an even odd example

---
+++ flannery_odd_example.py
@@ -0,0 +1,17 @@
+numbr = float(input("What number would you like to assess?"))  # could use regex to decide what to type this variable
+
+if type(numbr) == float and numbr % 2 != 0 and numbr % 2 != 1:
+    print("Your number contains a decimal.")
+
+if numbr % 2 == 1:
+    print("Your number is odd.")
+    if numbr % 3 == 0:
+        print("Your number is also divisible by 3 but not 6")
+
+elif numbr % 2 == 0:
+    print("Your number is even.")
+    if numbr % 3 == 0:
+        print("Your number is also divisible by 3 and 6")
+
+
+
