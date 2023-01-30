4fa5fd5120dfb29c637a0be120c1e3ea289e0f97
Seamus Flannery <seamusmflannery@gmail.com>
2023-01-30 10:45:22 -0500

    messed around with a thing

--- flannery_odd_example.py
+++ flannery_odd_example.py
@@ -1,4 +1,5 @@
-numbr = float(input("What number would you like to assess?"))  # could use regex to decide what to type this variable
+numbr = float(input("What number would you like to assess?"))
+# could use regex to decide what to type this variable
 
 if type(numbr) == float and numbr % 2 != 0 and numbr % 2 != 1:
     print("Your number contains a decimal.")
