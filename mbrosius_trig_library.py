51fed02ecb195e45e4d89ab9d2038446f3d5e964
Brosius Maddy <mbrosius@brynmawr.edu>
2023-02-01 11:29:15 -0500

    trig library in progress

---
+++ mbrosius_trig_library.py
@@ -0,0 +1,14 @@
+#library of trig functions
+
+def factorial(x):
+    if(x==1)
+        fac=1
+    else:
+        fac=x*factorial(x-1)
+    return fac
+
+def sin(x):
+    sum=0
+    for n in range(0,Nmax+1)
+        
+
