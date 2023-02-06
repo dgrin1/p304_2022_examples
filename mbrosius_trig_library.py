16964583c6f188af14268282731e99d291548a30
Maddy Brosius <mbrosius@brynmawr.edu>
2023-02-06 11:29:17 -0500

    trig library in progress

--- mbrosius_trig_library.py
+++ mbrosius_trig_library.py
@@ -1,7 +1,10 @@
 #library of trig functions
 
+import numpy as np
+Nmax = 65
+
 def factorial(x):
-    if(x==1)
+    if(x==1):
         fac=1
     else:
         fac=x*factorial(x-1)
@@ -9,6 +12,36 @@ def factorial(x):
 
 def sin(x):
     sum=0
-    for n in range(0,Nmax+1)
-        
+    for n in range(0,Nmax+1):
+        numerator=((-1)**n)*(x**(2*n+1))
+        int_want=2*n+1
+        denominator=factorial(int_want)
+        term=numerator/denominator
+        sum+=term
+    return sum
+print(sin(0.3))
 
+def cos(x):
+    sum=0
+    for n in range(0,Nmax+1):
+        numerator=((-1)**n)*(x**(2*n))
+        if (n==0):
+            denominator=1
+        else:
+            int_want=2*n
+            denominator=factorial(int_want)
+        term=numerator/denominator
+        sum+=term
+    return sum
+print(cos(0.3))
+
+def tan(x):
+    tan=sin(x)/cos(x)
+    return tan
+print(tan(0.3))
+
+def sec(x):
+
+def csc(x):
+
+def cot(x):
