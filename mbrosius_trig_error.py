282d6244f521e118e03a2f2e1ba8c1e1050a9924
Brosius Maddy <mbrosius@brynmawr.edu>
2023-02-13 11:30:55 -0500

    error

--- mbrosius_trig_error.py
+++ mbrosius_trig_error.py
@@ -4,14 +4,24 @@ from mbrosius_trig_library import *
 
 
 data = []
+data2 = []
+#error = []
 for n in range(0,100):
     temp=(2*np.pi/100)*n
-    data.append(tan(temp))
+#    error_temp=[cos(temp)-cos((2*np.pi/100)*n)]/cos(temp)
+    data.append(cos(temp))
+    data2.append(np.cos(temp))
+#    error.append(error_temp)
     
 x=np.linspace(0,2*np.pi,100)
 y=data
+y2=data2
+
 
 plt.ylim([-1.1,1.1])
 
-plt.scatter(x,y)
+plt.plot(x,y)
+plt.plot(x,y2)
+
+plt.plot
 plt.show()
