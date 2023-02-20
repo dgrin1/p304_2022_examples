cdd0fc1fbc2f282927f30189e3736d1082518b07
Seamus Flannery <seamusmflannery@gmail.com>
2023-02-20 11:19:16 -0500

    Updated my simpson's rule program to also plot simpson error. This is locked for sin. I want to figure out how to add flexible function integrals

--- flannery_simpsonsrule.py
+++ flannery_simpsonsrule.py
@@ -28,4 +28,30 @@ def simpson_int(N, x_min, x_max):
     return (1/3)*h*(integrand(x_min) + integrand(x_max) + 4 * odd_sum + 2 * even_sum)
 
 
+def trip_prime(x):
+    return -np.cos(x)
+
+
+def simpson_error(x_min, x_max, N):
+    x_range = x_max - x_min
+    h = x_range / N
+    error = (1/180) * h**4 * (trip_prime(x_min)-trip_prime(x_max))
+    return error
+
+
+def plot_simp_error(N_min, N_max, x_min, x_max, points=50):
+    n_space = np.linspace(N_min, N_max, points)
+    error_data = []
+    for N in n_space:
+        error_data.append(abs(simpson_error(x_min, x_max, N)))
+    plt.plot(n_space, error_data)
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlabel("N")
+    plt.ylabel("Simpson Error")
+    plt.title("Simpson Error vs. Slice number, x_min = " + str(x_min) + ", x_max = " + str(x_max))
+    plt.show()
+
+
 print("The integral of sin(x) from 0 to pi is ", simpson_int(1000, 0, np.pi))
+plot_simp_error(0, 1000, 0, np.pi, 1000)
