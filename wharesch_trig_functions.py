460f36257148d148d7f7026c83f9795f7f386493
Your Name <you@example.com>
2023-02-22 11:23:22 -0500

    submitting completed trig

--- wharesch_trig_functions.py
+++ wharesch_trig_functions.py
@@ -1,17 +1,17 @@
 import numpy as np
 nMax=9
 
-def factorial (x):
-    if(x==1 or x==0):
+def factorial (i):
+    if(i==1 or i==0):
         fac=1
     else:
-        fac=x*factorial(x-1)
+        fac=i*factorial(i-1)
     return fac
 
-def sin_wharesch(x):
+def sin_wharesch(x,nMax):
     sum=0
     for n in range(0,nMax+1):
-        numerator=((-1)**n)*np.power(x,2*n+1)
+        numerator=(np.power(-1,n))*np.power(x,2*n+1)
         int_want=2*n+1
         denominator=factorial(int_want)
         term=numerator/denominator
@@ -19,17 +19,17 @@ def sin_wharesch(x):
     return sum
 
 def cos_wharesch(x):
-    return sin_wharesch(np.pi/2-x)
+    return sin_wharesch(np.pi/2-x,9)
     
 
 def tan_wharesch(x):
-    return sin_wharesch(x)/cos_wharesch(x)
+    return sin_wharesch(x,9)/cos_wharesch(x)
 
 def sec_wharesch(x):
     return 1/cos_wharesch(x)
 
 def csc_wharesch(x):
-    return 1/sin_wharesch(x)
+    return 1/sin_wharesch(x,9)
 
 def cot_wharesch(x):
     return 1/tan_wharesch(x)
