338af888d27094560bff775d5361c2e87b4bfdbd
Your Name <you@example.com>
2023-03-15 11:24:34 -0400

    fixing newton example

--- newton_wharesch.py
+++ newton_wharesch.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def iteration(x0):
-    if abs(f(x0))>0.1:
+    if abs(f(x0))>0.0001:
         return iteration(x0-f(x0)/derivative(x0))
     else:
         print("This function reaches 0 at "+str(x0))
@@ -13,13 +13,13 @@ def f(x):
 def derivative(x):
     return -np.sin(x)-np.exp(x)
 
-#0s are at x=0 and x~-1.29, and the maximum is at ~-0.55, so try values on either side of peak to find 0s
-iteration(5)
+#0s are at x=0 and x~-1.29 then recurring negative values, and the maximum between the two rightmost 0s is at ~-0.55, so try values on either side of peak to find 0s
+iteration(-20)
 
-xrange=np.linspace(-2,1,500)
+xrange=np.linspace(-21,1,500)
 frange=[]
 
-for i in np.linspace(-2,1,500):
+for i in np.linspace(-21,1,500):
     frange.append(f(i))
 
 plt.plot(xrange,frange)
