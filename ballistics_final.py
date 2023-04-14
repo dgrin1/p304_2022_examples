41b4d2cfea6e77ed9a407b837116a3c1800ffc57
bbergerson <ben@bergerson.org>
2023-04-14 13:05:52 -0400

    ballistics rk4

--- ballistics_final.py
+++ ballistics_final.py
@@ -12,30 +12,32 @@ def F_v(v):
     F_v = 0.0039 + (0.0058/(1+(np.exp(v-35))/5))
     return(F_v)
 
-def pitch(position,velocity,theta,phi):
-    x = position[0]
-    y = position[1]
-    z = position[2]
-    v_x = velocity[0]
-    v_y = velocity[1]
-    v_z = velocity[2]
+def pitch(r,t):
+    x = r[0]
+    y = r[1]
+    z = r[2]
+    v_x = r[3]
+    v_y = r[4]
+    v_z = r[5]
+    theta = r[6]
+    phi = r[7]
     speed = np.sqrt(v_x**2+v_y**2+v_z**2)
     dx = v_x
     dy = v_y
     dz = v_z
-    position_sol = [dx,dy,dz]
     dv_x = -F_v(speed)*v_x + B*omega*(v_z*sin(phi)-v_y*cos(phi))
     dv_y = -F_v(speed)*v_y + B*omega*v_x*cos(phi)
     dv_z = -g-F_v(speed)*v_z + B*omega*v_x*sin(phi)
-    velocity_solution = [dv_x,dv_y,dv_z]
-    return (position_sol,velocity_solution)
+    r = np.array([dx,dy,dz,dv_x,dv_y,dv_z,theta,phi],float)
+    return (r)
 
 a = 0.0
-b = 60
-N = 10000
+b = 60.0
+N = 100000
 h = (b-a)/N
 
 tpoints = arange(a,b,h)
+ypoints = []
 xpoints = []
 
 # initial conditions for fastball
@@ -45,26 +47,27 @@ phi = 225 * np.pi/180
 position = [0,0,0]
 velocity = [v_0*cos(theta),0,v_0*sin(theta)]
 
+r = position + velocity
+r.append(theta)
+r.append(phi)
+
+r = np.array(r,float)
+# r = [x positon, y position, z position, x velocity, y velocity, z velocity, theta, phi]
 
 for t in tpoints:
-    xpoints.append(x)
-    k1 = h*f(x,t)
-    k2 = h*f(x+0.5*k1,t+0.5*h)
-    k3 = h*f(x+0.5*k2,t+0.5*h)
-    k4 = h*f(x+k3,t+h)
-    x += (k1+2*k2+2*k3+k4)/6
+    xpoints.append(r[0])
+    ypoints.append(r[1])
+    k1 = h*pitch(r,t)
+    k2 = h*pitch(r+0.5*k1,t+0.5*h)
+    k3 = h*pitch(r+0.5*k2,t+0.5*h)
+    k4 = h*pitch(r+k3,t+h)
+    r += (k1+2*k2+2*k3+k4)/6
 
+plot(xpoints,ypoints)
+xlabel("x")
+ylabel("y")
+show()
 
-N_o = 1000000
-def g(x):
-    return N_o*np.exp(-l*x)
-
-points = np.linspace(a,b,20)
-ypoints = [g(x) for x in points]
-
-plot(points,ypoints,"o")
-xlabel("theta")
-ylabel("y(t)")
 
 plot(tpoints,xpoints)
 xlabel("theta")
