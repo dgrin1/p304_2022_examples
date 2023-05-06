b7ca5dededfe508de323aedd99cb68f1f0e18025
StefanyDubon <114364813+StefanyDubon@users.noreply.github.com>
2023-05-06 08:21:23 -0400

    example commit from stefany

---
+++ gausselimstefany.py
@@ -0,0 +1,56 @@
+from numpy import array,empty
+from numpy.linalg import inv,solve
+from numpy import copy,dot
+import numpy as np
+#
+A = array([[ 2,  1,  4,  1 ],
+            [ 3,  4, -1, -1 ],
+            [ 1, -4,  1,  5 ],
+            [ 2, -2,  1,  3 ]], float)
+v = array([ -4, 3, 9, 7 ],float)
+N = len(v)
+
+
+
+
+# A = array([[ 4,  -1, -1,  -1 ],
+#            [ -1,  3, 0, -1 ],
+#            [ -1, 0,  3,  -1 ],
+#            [ -1, -1,  -1,  4 ]], float)
+# v = array([ 5, 0, 5, 0 ],float)
+# N = len(v)
+
+
+B=copy(A)
+vold=copy(v)
+
+i=0
+while i<len(A):
+    if A[i,i] == 0:
+        orig_array = copy(A[i,:])
+        A[i,:] = A[np.argmax(A[:,i]),:]
+        A[np.argmax(A[:,i]),:] = orig_array
+    i+= 1
+
+
+# Gaussian elimination
+for m in range(N):
+    # Divide by the diagonal element
+    div = A[m,m]
+    A[m,:] /= div
+    v[m] /= div
+
+    # Now subtract from the lower rows
+    for i in range(m+1,N):
+        mult = A[i,m]
+        A[i,:] -= mult*A[m,:]
+        v[i] -= mult*v[m]
+    
+# Backsubstitution
+x = empty(N,float)
+for m in range(N-1,-1,-1):
+    x[m] = v[m]
+    for i in range(m+1,N):
+        x[m] -= A[m,i]*x[i]
+
+print(x)
